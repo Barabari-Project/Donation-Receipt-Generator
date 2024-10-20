1e87e10590c508a5d789617bd0bb5b574478cf39
drumil32 <akheniad@gmail.com>
2024-10-20 19:29:40 +0530

    fix: google auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,7 +42,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.BACKEND_BASE_URL + '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     const userData = profile._json;
     if (process.env.SOS_EMAIL == userData.email || process.env.RAKSHA_EMAIL == userData.email) {
