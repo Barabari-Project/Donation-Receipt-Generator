77a2c6b3030a2bcd41f98e216c5c4f1fbe101c0d
drumil32 <akheniad@gmail.com>
2024-12-18 00:06:06 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: process.env.BACKEND_BASE_URL + '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
     // const userData = profile._json;
