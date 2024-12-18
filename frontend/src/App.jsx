6845f915b53adeca98ea89050dc17d8d8ce1ffdf
drumil32 <akheniad@gmail.com>
2024-12-18 10:30:11 +0530

    fix 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,19 @@ import axios from 'axios';
 import Hero from './pages/Hero';
 import Footer from './pages/Footer';
 import Login from './pages/Login';
+import Cookies from 'js-cookie';
 
 function App() {
-
+  console.log(Cookies.get('connect.sid'))
+  const str = `connect.sid=${Cookies.get('connect.sid')}`
   useEffect(() => {
     const awakeServer = async () => {
       try {
         await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/health`, {
           withCredentials: true,
+          headers: {
+            Cookie: str,
+          },
         });
       } catch (error) {
       }
